a83e22c54868dfcfe0279a22c3e46afe9c122d2e
Gourav Khurana <gouravkhurana310@gmail.com>
2020-11-21 23:28:17 +0530

    cmit3

--- src/components/index.js
+++ src/components/index.js
@@ -8,14 +8,14 @@
 function updateStructure(rec1,rec2){
 	if(contains(rec1,rec2)){
 		const relativeDim = relative(rec1,rec2);
-		return {...rec1,children:[relativeDim]}
+		return {...rec1,children:[relativeDim]};
 	}
 	else if(contains(rec2,rec1)){
 		const relativeDim = relative(rec2,rec1);
-		return {...rec2,children:[relativeDim]}
+		return {...rec2,children:[relativeDim]};
 	}
 	else{
-		return {...rec1}
+		return {...rec1};
 	}
 
 	//write your code
@@ -64,7 +64,7 @@ const T=0;
 function normalize(rec){
 	return {
 		x1:rec.top ? parseInt(rec.top): (T-(parseInt(rec.bottom)+parseInt(rec.height))),
-		y1: rec.left ? parseInt(rec.left):(w-(parseInt(rec.right)+parseInt(rec.width))),
+		y1: rec.left ? parseInt(rec.left):(W-(parseInt(rec.right)+parseInt(rec.width))),
 		x2: rec.bottom ? (T-parseInt(rec.bottom)): (parseInt(rec.top)+parseInt(rec.height)),
 		y2: rec.right ? (W-parseInt(rec.right)): (parseInt(rec.left)+parseInt(rec.width))
 	}
