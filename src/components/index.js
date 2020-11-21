c517a68a8b4a528280f4ac26c803c531ef16e753
Gourav Khurana <gouravkhurana310@gmail.com>
2020-11-21 23:54:13 +0530

    cmnit4

--- src/components/index.js
+++ src/components/index.js
@@ -8,30 +8,29 @@
 function updateStructure(rec1,rec2){
 	if(contains(rec1,rec2)){
 		const relativeDim = relative(rec1,rec2);
-		return {...rec1,children:[relativeDim]};
+		return {...rec1, children:[relativeDim]};
 	}
 	else if(contains(rec2,rec1)){
 		const relativeDim = relative(rec2,rec1);
-		return {...rec2,children:[relativeDim]};
+		return {...rec2, children:[relativeDim]};
 	}
 	else{
 		return {...rec1};
 	}
 
-	//write your code
 }
-function relative(recA,recB){
+function relative(recA , recB){
 	const recAn= normalize(recA);
 	const recBn = normalize(recB);
 
-	const res ={
+	const res = {
 		children : recB.children
 	}
 	if(recB.top){
-		res.top= `${recBn.x1-recAn.x1}px`;
+		res.top= `${recBn.x1 - recAn.x1}px`;
 	}
 	if(recB.left){
-		res.left= `${recBn.y1-recAn.y1}px`;
+		res.left= `${recBn.y1 - recAn.y1}px`;
 	}
 	if(recB.height){
 		res.height = recB.height;
@@ -40,17 +39,18 @@ function relative(recA,recB){
 		res.width=recB.width;
 	}
 	if(recB.bottom){
-		res.bottom= `${recAn.x2-recBn.x2}px`
+		res.bottom= `${recAn.x2 - recBn.x2}px`
 	}
-	if(recB.left){
-		res.left= `${recAn.y2-recBn.y2}px`
+	if(recB.right){
+		res.right= `${recAn.y2 - recBn.y2}px`
 	}
 	return res;
 }
 function contains(recA,recB){
 	const recAn= normalize(recA);
 	const recBn = normalize(recB);
-	if( recAn.x1 <= recBn.x1
+
+	if( recAn.x1 <= recBn.x1 
 		&& recAn.y1 <= recBn.y1 
 		&& recAn.x2 >= recBn.x2
 		&& recAn.y2 >= recBn.y2
@@ -59,14 +59,14 @@ function contains(recA,recB){
 		}
      return false;	
 }
-const W= 0;
-const T=0;
+const W= 100000;
+const T=100000;
 
 
 function normalize(rec){
 	return {
-		x1:rec.top ? parseInt(rec.top): (T-(parseInt(rec.bottom)+parseInt(rec.height))),
-		y1: rec.left ? parseInt(rec.left):(W-(parseInt(rec.right)+parseInt(rec.width))),
+		x1: rec.top ? parseInt(rec.top): (T- (parseInt(rec.bottom)+parseInt(rec.height))),
+		y1: rec.left ? parseInt(rec.left):(W- (parseInt(rec.right)+parseInt(rec.width))),
 		x2: rec.bottom ? (T-parseInt(rec.bottom)): (parseInt(rec.top)+parseInt(rec.height)),
 		y2: rec.right ? (W-parseInt(rec.right)): (parseInt(rec.left)+parseInt(rec.width))
 	}
